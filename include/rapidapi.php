ba1ff0f0c02d99a25de5e1939c5e75aa05928af5
Christian Heiler <cheiler79@gmail.com>
2019-01-02 13:59:59 +0200

    Update Rapid API to include Download

--- include/rapidapi.php
+++ include/rapidapi.php
@@ -12,7 +12,7 @@ class rapidapi
     private $sharedSecret="";
     private $baseUrl= "https://api.ean.com";
     private $bookingUrl = " https://book.api.ean.com";
-    private $version = 1;
+    private $version = "2.1";
     private $customerIP = "128.0.0.1";
     private $xForward = "128.0.0.2";
     private $arrival;
@@ -25,9 +25,11 @@ class rapidapi
     private $sales_environment = "HOTEL_ONLY";
     private $sortType = "PREFERRED";
     private $userAgent = "Don.t tellU 5.3";
+    private $downloadPath = "../files/";
 
     function __construct(){
         $this->debug = true;
+        $this->downloadPath = trim($this->downloadPath);
     }
 
     /**
@@ -160,7 +162,256 @@ class rapidapi
 
     }
 
+    /**
+     * @param array $propertyId
+     * @param string $locale
+     */
+    public function property(array $propertyId, $locale = "en-US"){
+        $method="GET";
+        $path="files/properties/catalog?language=$language";
+    }
 
+
+
+
+    /**
+     * @name contentCatalog
+     * @public
+     * @return object
+     */
+    public function contentCatalog($language = "en-US")
+    {
+        $method="GET";
+        $path="files/properties/catalog?language=$language";
+        if($response = $this->apiWrapper($method,$path)){
+            $this->debugger("Response retrieved");
+            $this->debugger(print_r($response, true));
+        } else {
+            $this->debugger("Response Error: Properties Catalog");
+            exit(503|"Response Error");
+        }
+
+        return $response;
+
+    }
+
+
+    /**
+     * @name contentComplete
+     * @public
+     * @return object
+     */
+    public function contentComplete($language = "en-US")
+    {
+        //TODO: language entry validation
+        $method="GET";
+        $path="files/properties/content?language=$language";
+        if($response = $this->apiWrapper($method,$path)){
+            $this->debugger("Response retrieved");
+            $this->debugger(print_r($response, true));
+        } else {
+            $this->debugger("Response Error: Properties Catalog");
+            exit(503|"Response Error");
+        }
+
+        return $response;
+
+    }
+
+   /**
+     * @name download
+     * @public
+     * @param $url
+     * @param $file
+     * @param $output Boolean Is output true of false
+     * @param $outputSize Integer output every X bytes
+     * @return boolean
+     */
+    public function download($url, $file, $output = false, $outputSize = 17000000)
+    {
+        //validate URL
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            $this->debugger("Incorrect URL: ".$url);
+            return false;
+        }
+        //create download directory
+        if (!file_exists($this->downloadPath)) {
+            mkdir($this->downloadPath, 0777, true);
+        }
+        $path=$this->downloadPath.$file;
+
+
+        /* if (file_put_contents($path, file_get_contents($url))){
+            return true;
+        }
+        */
+
+        if($this->copyfile_chunked($url, $path, $output, $outputSize)){
+            return true;
+        }
+
+
+        return false;
+    }
+
+
+    /**
+     * @name: copyfile_chunked
+     * Copy remote file over HTTP one small chunk at a time.
+     *
+     * @param $infile String The full URL to the remote file
+     * @param $outfile String The path where to save the file
+     * @param $output Boolean Is output true of false
+     * @param $outputSize Integer output every X bytes
+     * @return boolean
+     */
+    private function copyfile_chunked($infile, $outfile, $output = false, $outputSize = 17000000) {
+        $chunksize = 10 * (1024 * 1024); // 10 Megs
+
+        /**
+         * parse_url breaks a part a URL into it's parts, i.e. host, path,
+         * query string, etc.
+         */
+        $parts = parse_url($infile);
+        $i_handle = fsockopen($parts['host'], 80, $errstr, $errcode, 5);
+        $o_handle = fopen($outfile, 'wb');
+
+        if ($i_handle == false || $o_handle == false) {
+            return false;
+        }
+
+        if (!empty($parts['query'])) {
+            $parts['path'] .= '?' . $parts['query'];
+        }
+
+        /**
+         * Send the request to the server for the file
+         */
+        $request = "GET {$parts['path']} HTTP/1.1\r\n";
+        $request .= "Host: {$parts['host']}\r\n";
+        $request .= "User-Agent: Mozilla/5.0\r\n";
+        $request .= "Keep-Alive: 115\r\n";
+        $request .= "Connection: keep-alive\r\n\r\n";
+        fwrite($i_handle, $request);
+
+        /**
+         * Now read the headers from the remote server. We'll need
+         * to get the content length.
+         */
+        $headers = array();
+        
+        while(!feof($i_handle)) {
+            $line = fgets($i_handle);
+            if ($line == "\r\n") break;
+            $headers[] = $line;
+        }
+
+        /**
+         * Look for the Content-Length header, and get the size
+         * of the remote file.
+         */
+        $length = 0;
+        foreach($headers as $header) {
+            if (stripos($header, 'Content-Length:') === 0) {
+                $length = (int)str_replace('Content-Length: ', '', $header);
+                break;
+            }
+        }
+
+        /**
+         * Start reading in the remote file, and writing it to the
+         * local file one chunk at a time.
+         */
+        $cnt = 0;
+        $counter = 0;
+        $old = 0;
+        while(!feof($i_handle)) {
+            $buf = '';
+            $buf = fread($i_handle, $chunksize);
+            $bytes = fwrite($o_handle, $buf);
+            if ($bytes == false) {
+                return false;
+            }
+            $cnt += $bytes;
+            //TODO: work on display only for debug;
+            if ($output){
+
+                $divisor = $outputSize; //for update speed...
+                if(round($cnt/$divisor) !== $old){
+                    //echo round($cnt/1000000,2)."MB of ".round($length/1000000)."MB\r";
+                    $this->terminal_statusbar($cnt,$length,50);
+                    $old = round($cnt/$divisor);
+                }
+            }
+
+            	
+            
+            /**
+             * We're done reading when we've reached the conent length
+             */
+            if ($cnt >= $length) break;
+        }
+
+        fclose($i_handle);
+        fclose($o_handle);
+        return $cnt;
+    }//END copyfile_chunked
+
+
+    /**
+     * @param $current
+     * @param $max
+     * @param $length
+     */
+    private function terminal_statusbar($current, $max, $length){
+        //calculate how many percentage points 1 unit is
+        $filledBlocks = round($current*$length/$max);
+        $emptyBlocks = $length - $filledBlocks;
+
+        echo "Downloading: |";
+
+        for ($x = 0; $x <= $filledBlocks; $x++) {
+            echo "█";
+        }
+        for ($x = 0; $x <= $emptyBlocks; $x++) {
+            echo " ";
+        }
+        echo "| ";
+        echo round($current/1000000,2)."MB of ".round($max/1000000)."MB       \r";
+
+    }
+
+
+
+    /**
+     * @name uncompress
+     * @public
+     * @param $source
+     * @param $target
+     * @return bool
+     */
+    public function uncompress($source, $target){
+
+        $srcFile = $this->downloadPath.trim($source);
+        $toFile = $this->downloadPath.trim($target);
+
+        if(!file_exists($srcFile)){
+            $this->debugger("Source file does not exist");
+            return false;
+        }
+
+        $sfp = gzopen($srcFile, "rb");
+        $fp = fopen($toFile, "w");
+
+        while ($string = gzread($sfp, 4096)) {
+            fwrite($fp, $string, strlen($string));
+        }
+        gzclose($sfp);
+        fclose($fp);
+        return true;
+    }
+    
+    
 
 
     /**Getters and Setters
